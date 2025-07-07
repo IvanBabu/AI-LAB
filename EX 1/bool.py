3ffb7544689f962cb34e5ad2ff765cda8004a357
IvanBabu <ivanbabu1@gmail.com>
2025-07-07 11:23:44 +0530

    Add files via upload

---
+++ EX 1/bool.py
@@ -0,0 +1,10 @@
+is_sunny=True
+have_umbrella=False
+if(is_sunny or have_umbrella):
+	print("Yes, you may go out")
+else:
+	print("No, you may not go out")
+if not is_sunny:
+	print("Yes, you should carry an umbrella!")
+else:
+	print("You may not carry umberlla")
