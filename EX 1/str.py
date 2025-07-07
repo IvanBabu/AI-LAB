3412f650ad041ad78318a3924858e17d86510320
IvanBabu <ivanbabu1@gmail.com>
2025-07-07 11:23:25 +0530

    Add files via upload

---
+++ EX 1/str.py
@@ -0,0 +1,14 @@
+s=input("Enter the string")
+c=0
+for i in s:
+	c+=1
+print("The total no of char are:",c)
+print("The string after switching", s)
+print("Upper case letters", s.upper())
+print("Lower case letters", s.lower())
+st=s.replace('','_')
+print("New string", str)
+if 'Python' in s:
+	print("The word 'Python' is present")
+else:
+	print("The word 'Python' is not present ")
